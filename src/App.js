b63d53b58581c33eccbde58825af9e1678fb8401
Amitkumar Radheshyam Yadav <yadav.am@northeastern.edu>
2022-12-31 02:48:16 -0500

    adding inputs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import {useState} from 'react';
 
 
 function App() {
-  return (
-    <div>
+  const[username, setUsername]=useState('');
+  const[age, setAge]=useState('');
 
+  const usernameHandler=(e)=>{
+    setUsername(e.target.value);
+  }
+
+  const ageHandler=(e)=>{
+    setAge(e.target.value)
+  }
+
+  return (
+    <div style={{display:'flex', flexDirection:"column", alignItems:"center"}}>
+      <label>Username</label>
+      <input onChange={usernameHandler} style={{width:"20%"}}/>
+    
+      <label>Age</label>
+      <input onChange={ageHandler} style={{width:"20%"}}/>
+
+      
+      
     </div>
   );
 }
